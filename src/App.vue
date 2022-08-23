350a8f0661e597bd49089709c785d2f2e9614878
nadahkhaledd <nadahkhaledd@gmail.com>
2022-08-24 00:49:38 +0200

    added records and puzzles components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Puzzles @puzzle-changed="selectedPuzzleId = $event" />
+    <Records />
+    <SliderPuzzle :puzzleId="selectedPuzzleId" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SliderPuzzle from "./components/SliderPuzzle.vue";
+import Puzzles from "./components/Puzzles.vue";
+import Records from "./components/Records.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SliderPuzzle,
+    Puzzles,
+    Records,
+  },
+  data() {
+    return {
+      selectedPuzzleId: "cut-pink",
+    };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Records.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Records</h1>
+    <button @click="getRecords">Refresh</button>
+    <div v-for="(r, index) of records" :key="index">{{index + 1}} - {{r.elapsedTime}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Records",
+  data() {
+    return {
+      records: [],
+    };
+  },
+  created() {
+    this.getRecords();
+  },
+  methods: {
+    getRecords() {
+      const records = JSON.parse(localStorage.getItem("records")) || [];
+      this.records = records;
+    },
+  },
+};
+</script>
